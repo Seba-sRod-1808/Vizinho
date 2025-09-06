8960f1f2f257c136d8f3927c0c7174daa7157987
DarkFox90 <carlospozuelos3@gmail.com>
2025-09-05 21:17:00 -0600

    agregado formulario de reportes en forms.py

--- core/forms.py
+++ core/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .models import Reporte
 
 class LoginForm(forms.Form):
     username = forms.CharField(label="Usuario", max_length=100)
@@ -9,3 +10,14 @@ class LoginForm(forms.Form):
         if not data:
             raise forms.ValidationError("El nombre de usuario es obligatorio")
         return data
+    
+class ReporteForm(forms.ModelForm):
+    class Meta:
+        model = Reporte
+        fields = ["_titulo", "_descripcion", "_ubicacion"]
+
+    def clean__titulo(self):
+        titulo = self.cleaned_data["_titulo"]
+        if len(titulo) < 5:
+            raise forms.ValidationError("El título debe tener al menos 5 caracteres.")
+        return titulo
