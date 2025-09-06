49cbdfdbb7f599bcc1e15a8ce01c2c5552de8b31
Seba-sRod-1808 <73550691+Seba-sRod-1808@users.noreply.github.com>
2025-09-06 10:17:09 -0600

    Agregadas las vistas/controladores para los reportes de usuarios en views.py

--- core/views.py
+++ core/views.py
@@ -1,8 +1,11 @@
 from django.views import View
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
-from .forms import LoginForm
-
+from .forms import LoginForm, ReporteForm
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Reporte
 
 class LoginView(View):
     template_name = "login.html"
@@ -19,7 +22,7 @@ class LoginView(View):
             user = authenticate(request, username=username, password=password)
             if user:
                 login(request, user)
-                return redirect("dashboard")  # ajustaremos después
+                return redirect("dashboard") # aun no implementado // equipo de fabric y joseph
             else:
                 form.add_error(None, "Usuario o contraseña incorrectos")
         return render(request, self.template_name, {"form": form})
@@ -29,3 +32,36 @@ class LogoutView(View):
     def get(self, request):
         logout(request)
         return redirect("login")
+
+class ReporteListView(LoginRequiredMixin, ListView):
+    model = Reporte
+    template_name = "reportes/lista_reportes.html"
+    context_object_name = "reportes"
+
+    def get_queryset(self):
+        return Reporte.objects.filter(_vecino=self.request.user)
+
+
+class ReporteCreateView(LoginRequiredMixin, CreateView):
+    model = Reporte
+    form_class = ReporteForm
+    template_name = "reportes/crear_reporte.html"
+    success_url = reverse_lazy("lista_reportes")
+
+    def form_valid(self, form):
+        form.instance._vecino = self.request.user
+        return super().form_valid(form)
+
+
+class ReporteUpdateView(LoginRequiredMixin, UpdateView):
+    model = Reporte
+    form_class = ReporteForm
+    template_name = "reportes/editar_reporte.html"
+    success_url = reverse_lazy("lista_reportes")
+
+
+class ReporteDeleteView(LoginRequiredMixin, DeleteView):
+    model = Reporte
+    template_name = "reportes/eliminar_reporte.html"
+    success_url = reverse_lazy("lista_reportes")
+
