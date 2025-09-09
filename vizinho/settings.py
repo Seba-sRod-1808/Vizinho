3de658d6596b082f4b56e3e3dd0bb7067c502087
Sebastián Rodas <sebasrod1808@gmail.com>
2025-09-08 19:03:46 -0600

    Hotfix - Agregados metodos de redireccionamiento de vistas protegidas a settings.py

--- vizinho/settings.py
+++ vizinho/settings.py
@@ -123,3 +123,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'core.Usuario'
+
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'dashboard'
+LOGOUT_REDIRECT_URL = 'login'
